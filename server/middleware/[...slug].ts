2443be2d64e24dc48a8fb3a0bea1c3966c23013f
devzota <111525617+devzota@users.noreply.github.com>
2023-05-08 21:11:33 +0700

    add test
    add test

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -16,4 +16,7 @@ export default defineEventHandler((event) => {
     if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
         sendRedirect(event,"https://today247.online"+event.req.url);
     }
+    if ('https://test-zt.vercel.app/taylor-swift-nashville-concert-forecast-storms-likely-sunday/' && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
+        sendRedirect(event,"https://atoz24h.info/how-camila-cabello-maintained-her-shaggy-waves-all-night-at-the-2023-grammys/");
+    }
 })
